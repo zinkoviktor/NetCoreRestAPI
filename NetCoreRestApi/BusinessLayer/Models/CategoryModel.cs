a678d3a490c811493ced870b09797f8422e4ca5d
Viktor <zinkoexp@gmail.com>
2020-05-15 10:06:44 +0300

    Added Category and Product models (#3)
    * Added CategoryModel class
    
    * Added ProductModel class

---
+++ NetCoreRestApi/BusinessLayer/Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BusinessLayer.Models
+{
+    public class CategoryModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ NetCoreRestApi/BusinessLayer/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BusinessLayer.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<CategoryModel> CategoryList { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableCount { get; set; }
+    }
+}
