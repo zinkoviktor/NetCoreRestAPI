41de17c64336e064c63b4f20294eb8d2de5245f4
Viktor Zinko <zinkoexp@gmail.com>
2020-07-01 17:12:05 +0300

    Moved all Name and Description fields to Base class

--- NetCoreRestApi/DataLayer/Entities/BaseEntity.cs
+++ NetCoreRestApi/DataLayer/Entities/BaseEntity.cs
@@ -3,6 +3,8 @@ namespace DataLayer.Entities
 {
     public class BaseEntity : IEntity
     {
-        public int Id { get; set; }        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- NetCoreRestApi/ServiceLayer/DataTransferObjects/CategoryDTO.cs
+++ NetCoreRestApi/ServiceLayer/DataTransferObjects/CategoryDTO.cs
@@ -3,7 +3,6 @@ namespace ServiceLayer.DataTransferObjects
 {
     public class CategoryDTO : BaseDto
     {        
-        public string Name { get; set; }
-        public string Description { get; set; }
+        
     }
 }
