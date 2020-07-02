1b1eca7bfe3f1c2e0107875e8080d8b9afd9065e
Viktor Zinko <zinkoexp@gmail.com>
2020-07-03 01:05:59 +0300

    converters refactoring

--- NetCoreRestApi/DataLayer/Converters/ProductModelConverter.cs
+++ NetCoreRestApi/DataLayer/Converters/ProductModelConverter.cs
@@ -1,27 +1,23 @@
-﻿using DataLayer.Models;
-using Common.Converter;
+﻿using Common.Converter;
+using Common.Converters;
 using DataLayer.Entities;
-using System.Collections.Generic;
+using DataLayer.Models;
+using System;
+using System.Linq.Expressions;
 
 namespace DataLayer.Converters
 {
-    public class ProductModelConverter : IConverter<ProductEntity, ProductModel>
+    public class ProductModelConverter : BaseConvertor<ProductEntity, ProductModel>
     {
         private readonly IConverter<CategoryEntity, CategoryModel> _categoryConverter;
-
+        
         public ProductModelConverter(IConverter<CategoryEntity, CategoryModel> categoryConverter)
         {
             _categoryConverter = categoryConverter;
         }
 
-        public ProductModel ConvertTo(ProductEntity productEntity)
-        {
-            if (productEntity == null)
-            {
-                return null;
-            }
-
-            var productModel = new ProductModel
+        public override Expression<Func<ProductEntity, ProductModel>> ConvertToExpression =>
+            (productEntity) => new ProductModel()
             {
                 Id = productEntity.Id,
                 Name = productEntity.Name,
@@ -31,33 +27,8 @@ namespace DataLayer.Converters
                 Price = productEntity.Price
             };
 
-            return productModel;
-        }
-
-        public ICollection<ProductModel> ConvertTo(ICollection<ProductEntity> productEntities)
-        {
-            var productModels = new List<ProductModel>();
-            if (productEntities == null)
-            {
-                return productModels;
-            }
-
-            foreach (var productEntity in productEntities)
-            {
-                productModels.Add(ConvertTo(productEntity));
-            }
-
-            return productModels;
-        }
-
-        public ProductEntity ConvertFrom(ProductModel productModel)
-        {
-            if (productModel == null)
-            {
-                return null;
-            }
-
-            var productEntity = new ProductEntity()
+        public override Expression<Func<ProductModel, ProductEntity>> ConvertFromExpression =>
+            (productModel) => new ProductEntity()
             {
                 Id = productModel.Id,
                 Name = productModel.Name,
@@ -66,25 +37,5 @@ namespace DataLayer.Converters
                 AvailableCount = productModel.AvailableCount,
                 Price = productModel.Price
             };
-
-            return productEntity;
-        }
-
-        public ICollection<ProductEntity> ConvertFrom(ICollection<ProductModel> productModels)
-        {
-            var productEntities = new List<ProductEntity>();
-
-            if(productModels == null)
-            {
-                return productEntities;
-            }
-
-            foreach (var productModel in productModels)
-            {
-                productEntities.Add(ConvertFrom(productModel));
-            }
-
-            return productEntities;
-        }
     }
 }
