10e31214504525079fbcdfed022534155d3c0f30
Viktor Zinko <zinkoexp@gmail.com>
2020-08-27 23:50:27 +0300

    refactored ProductMockDbContext

--- NetCoreRestApi/DataLayer.EF/Configurations/CategoryConfiguration.cs
+++ NetCoreRestApi/DataLayer.EF/Configurations/CategoryConfiguration.cs
@@ -8,29 +8,23 @@ namespace DataLayer.EF.Configurations
     {
         public void Configure(EntityTypeBuilder<CategoryEntity> builder)
         {
-            builder.HasKey(c => c.Id);
-            builder.Property(c => c.Id).IsRequired();
-            builder.Property(c => c.Name).HasMaxLength(50).IsRequired();
-            builder.Property(c => c.Description).HasMaxLength(150);
-            builder.HasData(
-                new CategoryEntity()
-                {
-                    Id = 1,
-                    Name = "Laptops",
-                    Description = "Shop Laptops and find popular brands. Save money."
-                },
-                new CategoryEntity()
-                {
-                    Id = 2,
-                    Name = "Printers",
-                    Description = "The Best Printers for 2020."
-                },
-                new CategoryEntity()
-                {
-                    Id = 3,
-                    Name = "Sale",
-                    Description = "Shop all sale items"
-                });
+            builder
+                .HasKey(c => c.Id);
+            builder
+                .Property(c => c.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder
+                .Property(c => c.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+            builder
+                .Property(c => c.Description)
+                .HasMaxLength(150);
+            builder
+                .HasOne<ProductEntity>()
+                .WithMany(p => p.Categories)
+                .HasForeignKey(c => c.Id);
         }
     }
 }
--- NetCoreRestApi/DataLayer.EF/ProductMockDbContext.cs
+++ NetCoreRestApi/DataLayer.EF/ProductMockDbContext.cs
@@ -1,78 +1,86 @@
-﻿using DataLayer.EF.Converters;
-using DataLayer.EF.Entities;
-using DataLayer.Models;
+﻿using DataLayer.EF.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace DataLayer.EF
 {
-    public class ProductMockDbContext : ProductsDbContext
+    public class ProductMockDbContext : ProductsDbContext, IDbContext
     {
-        public ProductMockDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+        private List<ProductEntity> _productModels;
+        private List<CategoryEntity> _categoryEntities;
+
+        public ProductMockDbContext(DbContextOptions<ProductMockDbContext> options) : base(options)
         {
-            AddRange(GetProductsMock());
-            Save();
+            SetupCategories();
+            SetupProducts();            
+            Database.EnsureCreated();
         }
 
-        private IEnumerable<ProductEntity> GetProductsMock()
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var productModels = new List<ProductModel>
+            base.OnModelCreating(modelBuilder);           
+            modelBuilder
+                .Entity<CategoryEntity>()
+                .HasData(_categoryEntities);
+            modelBuilder                
+                .Entity<ProductEntity>()                
+                .HasData(_productModels);            
+        }
+
+        private void SetupProducts()
+        {
+            _productModels = new List<ProductEntity>
             {
-                new ProductModel()
+                new ProductEntity()
                 {
                     Id = 1,
                     Name = "HP 410",
-                    Description = "All-in-One Wireless Ink Tank Color Printer",
-                    CategoryList = new List<CategoryModel>()
-                    {
-                       new CategoryModel()
-                            {
-                                Id = 1,
-                                Name = "Laptops",
-                                Description = "Shop Laptops and find popular brands. Save money."
-                            }
-                    },
-                    Price = 90,
+                    Description = "All-in-One Wireless Ink Tank Color Printer",                      
+                    Price = 90,                   
                     AvailableCount = 9
                 },
-                new ProductModel()
+                new ProductEntity()
                 {
                     Id = 2,
                     Name = "Epson L3152",
-                    Description = "WiFi All in One Ink Tank Printer",
-                    CategoryList = new List<CategoryModel>()
-                    {
-                        new CategoryModel()
-                            {
-                                Id = 2,
-                                Name = "Printers",
-                                Description = "The Best Printers for 2020."
-                            }
-                    },
-                    Price = 60,
+                    Description = "WiFi All in One Ink Tank Printer",                    
+                    Price = 60,                    
                     AvailableCount = 19
                 },
-                new ProductModel()
+                new ProductEntity()
                 {
                     Id = 3,
                     Name = "Dell Inspiron 3583",
                     Description = "15.6-inch FHD Laptop",
-                    CategoryList = new List<CategoryModel>()
-                    {
-                        new CategoryModel()
-                            {
-                                Id = 3,
-                                Name = "Sale",
-                                Description = "Shop all sale items"
-                            }
-                    },
-                    Price = 50,
+                    Price = 50,                    
                     AvailableCount = 5
                 }
             };
+        }
 
-            var converter = new ProductModelConverter(new CategoryModelConverter());
-            return converter.ConvertFrom(productModels);
+        private void SetupCategories()
+        {
+            _categoryEntities = new List<CategoryEntity>()
+            {
+                new CategoryEntity()
+                {
+                    Id = 1,
+                    Name = "Laptops",
+                    Description = "Shop Laptops and find popular brands. Save money."
+                },
+                new CategoryEntity()
+                {
+                    Id = 2,
+                    Name = "Printers",
+                    Description = "The Best Printers for 2020."
+                },
+                new CategoryEntity()
+                {
+                    Id = 3,
+                    Name = "Sale",
+                    Description = "Shop all sale items"
+                }
+            };
         }
     }
 }
