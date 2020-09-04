0bf9d6965f7a3bb9b0321a72c0927333f11cbc36
Viktor Zinko <zinkoexp@gmail.com>
2020-09-04 10:41:53 +0300

    removed id manual seed

--- NetCoreRestApi/DataLayer.EF/ProductMockDbContext.cs
+++ NetCoreRestApi/DataLayer.EF/ProductMockDbContext.cs
@@ -27,7 +27,6 @@ namespace DataLayer.EF
             {
                 new ProductEntity()
                 {
-                    Id = 1,
                     Name = "HP 410",
                     Description = "All-in-One Wireless Ink Tank Color Printer",
                     Price = 90,                    
@@ -35,7 +34,6 @@ namespace DataLayer.EF
                 },
                 new ProductEntity()
                 {
-                    Id = 2,
                     Name = "Epson L3152",
                     Description = "WiFi All in One Ink Tank Printer",                    
                     Price = 60,                    
@@ -43,7 +41,6 @@ namespace DataLayer.EF
                 },
                 new ProductEntity()
                 {
-                    Id = 3,
                     Name = "Dell Inspiron 3583",
                     Description = "15.6-inch FHD Laptop",
                     Price = 50,                    
@@ -58,19 +55,16 @@ namespace DataLayer.EF
             {
                 new CategoryEntity()
                 {
-                    Id = 1,
                     Name = "Laptops",
                     Description = "Shop Laptops and find popular brands. Save money."
                 },
                 new CategoryEntity()
                 {
-                    Id = 2,
                     Name = "Printers",
                     Description = "The Best Printers for 2020."
                 },
                 new CategoryEntity()
                 {
-                    Id = 3,
                     Name = "Sale",
                     Description = "Shop all sale items"
                 }
