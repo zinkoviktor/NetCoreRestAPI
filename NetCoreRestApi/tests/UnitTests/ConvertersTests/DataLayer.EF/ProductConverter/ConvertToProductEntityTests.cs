cf1e59981b599d08ff546ba3a9f4a661776ffaec
Viktor Zinko <zinkoexp@gmail.com>
2020-09-10 11:41:28 +0300

    Added some test to productEntities tests

--- NetCoreRestApi/tests/UnitTests/ConvertersTests/DataLayer.EF/ProductConverter/ConvertToProductEntityTests.cs
+++ NetCoreRestApi/tests/UnitTests/ConvertersTests/DataLayer.EF/ProductConverter/ConvertToProductEntityTests.cs
@@ -17,8 +17,7 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
         CategoryModel categoryModelStub;
         ProductEntity productEntityStub;
         ProductCategoryEntity productCategoryEntityStub;
-        List<ProductEntity> productEntitiesStub;
-        IConverter<CategoryEntity, CategoryModel> categoryConverter;
+        List<ProductEntity> productEntitiesStub;        
 
         [TestInitialize]
         public void TestInitialize()
@@ -70,23 +69,26 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
                         Name = "",
                         Description = null,
                         AvailableCount = -9,
-                        Price = 9.999m
+                        Price = 9.999m                        
                 }
-            };
-
-            var mockCategotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
-            mockCategotyConverter
-                .Setup(x => x.ConvertTo(categoryEntityStub))
-                .Returns(categoryModelStub);
-            categoryConverter = mockCategotyConverter.Object;
+            };                       
         }
 
         [TestMethod]        
         public void CorrectClassTypeConvertedToModel()
         {
             // Arrange
-            productEntityStub.ProductCategoryEntities = new List<ProductCategoryEntity>() { productCategoryEntityStub };
-            var converter = new ProductModelConverter(categoryConverter);
+            productEntityStub.ProductCategoryEntities = new List<ProductCategoryEntity>() 
+            { 
+                productCategoryEntityStub 
+            };
+
+            var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
+            categotyConverter
+                .Setup(x => x.ConvertTo(It.IsAny<CategoryEntity>()))
+                .Returns(categoryModelStub);
+
+            var converter = new ProductModelConverter(categotyConverter.Object);
 
             // Act
             var actualProductModel = converter.ConvertTo(productEntityStub);
@@ -99,8 +101,17 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
         public void ConvertToModelIsNotNull()
         {
             // Arrange 
-            productEntityStub.ProductCategoryEntities = new List<ProductCategoryEntity>() { productCategoryEntityStub };
-            var converter = new ProductModelConverter(categoryConverter);
+            productEntityStub.ProductCategoryEntities = new List<ProductCategoryEntity>() 
+            { 
+                productCategoryEntityStub 
+            };
+
+            var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
+            categotyConverter
+                .Setup(x => x.ConvertTo(It.IsAny<CategoryEntity>()))
+                .Returns(categoryModelStub);
+
+            var converter = new ProductModelConverter(categotyConverter.Object);
 
             // Act
             var actualProductDto = converter.ConvertTo(productEntityStub);
@@ -141,8 +152,10 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
                 Category = categoryEntity,
                 CategoryId = categoryEntity.Id
             };
-            productEntity.ProductCategoryEntities = new List<ProductCategoryEntity>() { productCategoryEntity };
-            categoryEntity.ProductCategoryEntities = new List<ProductCategoryEntity>() { productCategoryEntity };
+            productEntity.ProductCategoryEntities = new List<ProductCategoryEntity>() 
+            { 
+                productCategoryEntity 
+            };
 
             var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
             categotyConverter
@@ -181,7 +194,11 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
             var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
             categotyConverter
                 .Setup(x => x.ConvertTo(It.IsAny<List<CategoryEntity>>()))
-                .Returns(new List<CategoryModel>() { categoryModel, categoryModelStub });
+                .Returns(new List<CategoryModel>() 
+                { 
+                    categoryModel, 
+                    categoryModelStub 
+                });
 
             var converter = new ProductModelConverter(categotyConverter.Object);
 
@@ -192,5 +209,109 @@ namespace UnitTests.ConvertersTests.DataLayer.EF.ProductConverter
             Assert.IsTrue(actualProductModel.CategoryList.Any(categoryModel => categoryModel.Name.Equals(name)));
         }
 
+        [TestMethod]        
+        public void CorrectConvertedToModelCollection()
+        {
+            // Arrange
+            var categoryMode = new CategoryModel();
+            foreach(var pe in productEntitiesStub)
+            {
+                pe.ProductCategoryEntities = new List<ProductCategoryEntity> 
+                { 
+                    productCategoryEntityStub 
+                };
+            }    
+
+            var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
+            categotyConverter
+                .Setup(x => x.ConvertTo(It.IsAny<List<CategoryEntity>>()))
+                .Returns(new List<CategoryModel>() 
+                { 
+                    categoryMode, 
+                    categoryModelStub 
+                });
+
+            var converter = new ProductModelConverter(categotyConverter.Object);
+
+            // Act
+            var actualProductModels = converter.ConvertTo(productEntitiesStub).ToList();
+
+            // Assert     
+            CollectionAssert.AllItemsAreInstancesOfType(actualProductModels, typeof(ProductModel));
+            CollectionAssert.AllItemsAreNotNull(actualProductModels);
+            Assert.AreEqual(actualProductModels.Count, 2, "Count of ProductModel items");
+            Assert.AreEqual(actualProductModels.First().CategoryList.ToList().Count, 2, "Count of CategoryModel items in ProductModel");
+        }
+
+        [TestMethod]
+        [DataRow(1, "name", "Description", 5, 9.99595)]
+        public void AllPropertiesConvertedToModelInCollection(int id, string name, string description, int count, double price)
+        {
+            // Arrange            
+            var productEntity = new ProductEntity()
+            {
+                Id = id,
+                Name = name,
+                Description = description,
+                AvailableCount = count,
+                Price = (decimal)price
+            };
+            var categoryEntity = new CategoryEntity()
+            {
+                Id = id,
+                Name = name,
+                Description = description
+            };
+            var categoryModel = new CategoryModel()
+            {
+                Id = id,
+                Name = name,
+                Description = description
+            };
+            var productCategoryEntity = new ProductCategoryEntity()
+            {
+                Product = productEntity,
+                ProductId = productEntity.Id,
+                Category = categoryEntity,
+                CategoryId = categoryEntity.Id
+            };
+            productEntity.ProductCategoryEntities = new List<ProductCategoryEntity>()
+            {
+                productCategoryEntity
+            };
+            productEntityStub.ProductCategoryEntities = new List<ProductCategoryEntity>()
+            {
+                productCategoryEntityStub
+            };
+            var productEntities = new List<ProductEntity>()
+            {
+                productEntityStub,
+                productEntity
+            };
+
+            var categotyConverter = new Mock<IConverter<CategoryEntity, CategoryModel>>();
+            categotyConverter
+                .Setup(x => x.ConvertTo(It.IsAny<List<CategoryEntity>>()))
+                .Returns(new List<CategoryModel>() 
+                { 
+                    categoryModel, 
+                    categoryModelStub 
+                });
+
+            var converter = new ProductModelConverter(categotyConverter.Object);
+
+            // Act
+            var actualProductModels = converter.ConvertTo(productEntities).ToList();
+            Assert.IsTrue(actualProductModels.Any(productModel => productModel.Name.Equals(name)),
+                "ProductModel is not found in converter collection");
+            var actualProductModel = actualProductModels.First(productModel => productModel.Name.Equals(name));
+
+            // Assert 
+            Assert.AreEqual(id, actualProductModel.Id, "Id property");
+            Assert.AreEqual(name, actualProductModel.Name, "Name property");
+            Assert.AreEqual(description, actualProductModel.Description, "Description property");
+            Assert.AreEqual(count, actualProductModel.AvailableCount, "AvailableCount property");
+            Assert.AreEqual((decimal)price, actualProductModel.Price, "Price property");
+        }
     }
 }
