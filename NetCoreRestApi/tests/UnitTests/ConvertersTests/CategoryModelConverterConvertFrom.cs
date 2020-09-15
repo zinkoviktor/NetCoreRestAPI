70c2c1ddfd9cc0ad416ffddb7ed541fee949de6e
Viktor Zinko <zinkoexp@gmail.com>
2020-09-15 23:09:23 +0300

    Updated CategoryConverters tests

--- NetCoreRestApi/tests/UnitTests/ConvertersTests/CategoryModelConverterConvertFrom.cs
+++ NetCoreRestApi/tests/UnitTests/ConvertersTests/CategoryModelConverterConvertFrom.cs
@@ -1,7 +1,9 @@
-﻿using DataLayer.EF.Converters;
+﻿using Common.Converter;
+using DataLayer.EF.Converters;
 using DataLayer.EF.Entities;
 using DataLayer.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,202 +12,79 @@ namespace UnitTests.ConvertersTests
     [TestClass]    
     public class CategoryModelConverterConvertFrom
     {
-        CategoryModelConverter converter;
-        CategoryModel categoryModel;
-        List<CategoryModel> categoryModels;
+        IConverter<CategoryEntity, CategoryModel> _converter;
+        IComparer _entityComparer;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            converter = new CategoryModelConverter();
-
-            categoryModel = new CategoryModel()
-            {
-                Id = 1,
-                Name = "Test Name",
-                Description = "Test Description",
-            };
-
-            categoryModels = new List<CategoryModel>()
-            {
-                categoryModel,
-                new CategoryModel()
-                {
-                     Id = 0,
-                     Name = "",
-                     Description = null,
-                }
-            };
-        }
-        
-        [TestMethod]         
-        public void CorrectClassTypeConvertedToCategoryEntity()
-        {
-            // Arrange
-            // Act
-            var actualCategoryEntity = converter.ConvertFrom(categoryModel);
-
-            // Assert            
-            Assert.IsInstanceOfType(actualCategoryEntity, typeof(CategoryEntity));           
-        }
-
-        [TestMethod]        
-        public void ConvertToCategoryEntityIsNotNull()
-        {
-            // Arrange 
-            // Act
-            var actualCategoryEntity = converter.ConvertFrom(categoryModel);
-
-            // Assert            
-            Assert.IsNotNull(actualCategoryEntity);
-        }
-
-        [TestMethod]
-        [Description("Is Id property value converted correct")]
-        [DataRow(1)]
-        [DataRow(0)]
-        [DataRow(-5)]
-        public void ConvertToCategoryEntityIdPropertyValueIsSameId(int expectedId)
-        {
-            // Arrange 
-            categoryModel.Id = expectedId;
-
-            // Act
-            var actualCategoryEntity = converter.ConvertFrom(categoryModel);
-
-            // Assert            
-            Assert.AreEqual(expectedId, actualCategoryEntity.Id);
-        }
-
-        [TestMethod]
-        [Description("Is name property value converted correct")]
-        [DataRow("Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,")]
-        [DataRow(null)]
-        [DataRow("")]
-        public void ConvertToCategoryEntityNamePropertyValueIsSameName(string expectedName)
-        {
-            // Arrange 
-            categoryModel.Name = expectedName;
-
-            // Act
-            var actualCategoryEntity = converter.ConvertFrom(categoryModel);
-
-            // Assert            
-            Assert.AreEqual(expectedName, actualCategoryEntity.Name);
-        }
-
-        [TestMethod]
-        [Description("Is Description property value converted correct")]
-        [DataRow("Description 1 !@~#$%^&*()_+=-\\||'\"?/.><,")]
-        [DataRow(null)]
-        [DataRow("")]
-        public void ConvertToCategoryEntityDescriptionPropertyValueIsSameDescription(string expectedDescription)
-        {
-            // Arrange 
-            categoryModel.Description = expectedDescription;
-
-            // Act
-            var actualCategoryEntity = converter.ConvertFrom(categoryModel);
-
-            // Assert            
-            Assert.AreEqual(expectedDescription, actualCategoryEntity.Description);
-        }
-
-        [TestMethod]
-        public void CorrectClassTypeConvertedToCategoryEntityCollection()
-        {
-            // Arrange
-            // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
-
-            // Assert            
-            Assert.IsInstanceOfType(actualCategoryEntities, typeof(IEnumerable<CategoryEntity>));
-        }
-
-        [TestMethod]
-        public void CorrectAllItemsTypesConvertedToCategoryEntityCollection()
-        {
-            // Arrange
-            // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
-
-            // Assert            
-            CollectionAssert.AllItemsAreInstancesOfType(actualCategoryEntities.ToList(), typeof(CategoryEntity));
-        }
-
-        [TestMethod]
-        public void ConvertToCategoryEntityCollectionIsNotNull()
-        {
-            // Arrange             
-
-            // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
-
-            // Assert            
-            Assert.IsNotNull(actualCategoryEntities);
+            _converter = new CategoryModelConverter();
+            _entityComparer = new CategoryEntityComparer();
         }
 
         [TestMethod]
         public void ConvertToCategoryEntityCollectionAllItemsAreNotNull()
         {
             // Arrange             
+            var categoryModel = new CategoryModel();
+            var categoryModels = new List<CategoryModel>()
+            {
+                categoryModel,
+                new CategoryModel()
+                {
+                     Id = 0,
+                     Name = "",
+                     Description = null
+                }
+            };
 
             // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
+            var actualCategoryEntities = _converter.ConvertFrom(categoryModels);
 
             // Assert            
             CollectionAssert.AllItemsAreNotNull(actualCategoryEntities.ToList());
         }
 
         [TestMethod]
-        [Description("Is Id property value converted correct")]
-        [DataRow(7)]
-        [DataRow(0)]
-        [DataRow(-5)]
-        public void ConvertToCategoryEntityIdPropertyValueInCollectionIsSameId(int expectedId)
+        [Description("Is Id property value converted correct")]       
+        public void ConvertToCategoryEntityIdPropertyValueInCollectionIsSameId()
         {
-            // Arrange 
-            categoryModels.Last().Id = expectedId;
+            // Arrange
+            var categoryModel = new CategoryModel()
+            {
+                Id = 1,
+                Name = "Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,",
+                Description = "Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,"
+            };
+            var categoryModel2 = new CategoryModel()
+            {
+                Id = 0,
+                Name = "",
+                Description = ""
+            };
+            var categoryModels = new List<CategoryModel>()
+            {
+                categoryModel, categoryModel2
+            };
+            var categoryEntity = new CategoryEntity()
+            {
+                Id = 1,
+                Name = "Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,",
+                Description = "Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,"
+            };
+            var categoryEntity2 = new CategoryEntity()
+            {
+                Id = 0,
+                Name = "",
+                Description = ""
+            };            
+            var expected = new List<CategoryEntity> { categoryEntity, categoryEntity2 };
 
             // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
+            var actual = _converter.ConvertFrom(categoryModels).ToList();
 
             // Assert            
-            Assert.AreEqual(expectedId, actualCategoryEntities.Last().Id);
-        }
-
-        [TestMethod]
-        [Description("Is name property value converted correct")]
-        [DataRow("Name 1 !@~#$%^&*()_+=-\\||'\"?/.><,")]
-        [DataRow(null)]
-        [DataRow("")]
-        public void ConvertToCategoryEntityNamePropertyValueInCollectionIsSameName(string expectedName)
-        {
-            // Arrange 
-            categoryModels.First().Name = expectedName;
-
-            // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
-
-            // Assert            
-            Assert.AreEqual(expectedName, actualCategoryEntities.First().Name);
-        }
-
-        [TestMethod]
-        [Description("Is Description property value converted correct")]
-        [DataRow("Description 1 !@~#$%^&*()_+=-\\||'\"?/.><,")]
-        [DataRow(null)]
-        [DataRow("")]
-        public void ConvertToCategoryEntityDescriptionPropertyValueInCollectionIsSameDescription(string expectedDescription)
-        {
-            // Arrange 
-            categoryModels.Last().Description = expectedDescription;
-
-            // Act
-            var actualCategoryEntities = converter.ConvertFrom(categoryModels);
-
-            // Assert            
-            Assert.AreEqual(expectedDescription, actualCategoryEntities.Last().Description);
+            CollectionAssert.AreEqual(expected, actual, _entityComparer);
         }
     }
 }
---
+++ NetCoreRestApi/tests/UnitTests/ConvertersTests/EntityComparer.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer.EF.Entities;
+using System.Collections;
+
+namespace UnitTests.ConvertersTests
+{
+    public class CategoryEntityComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            if(x.GetType() != typeof(CategoryEntity))
+            {
+                return -1;
+            }
+
+            if (y.GetType() != typeof(CategoryEntity))
+            {
+                return -1;
+            }
+
+            var entity1 = (CategoryEntity) x;
+            var entity2 = (CategoryEntity) y;
+            if (entity1.Id.Equals(entity2.Id) &&
+                entity1.Name.Equals(entity2.Name) &&
+                entity1.Description.Equals(entity2.Description))
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+    }
+}
