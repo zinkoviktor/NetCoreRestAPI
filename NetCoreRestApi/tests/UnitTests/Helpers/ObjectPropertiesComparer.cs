cab950dab8afb810de65273f2e2e2857b9cdc353
Viktor Zinko <zinkoexp@gmail.com>
2020-10-11 00:07:41 +0300

    Added property comparer

---
+++ NetCoreRestApi/tests/UnitTests/Helpers/ObjectPropertiesComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Helpers
+{
+    public static class ObjectPropertiesComparer
+    {
+        public static bool AreEqual(object a, object b)
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+          
+            if (a == null || b == null) 
+            { 
+                return false; 
+            }
+
+            if (a.GetType() != b.GetType())
+            {
+                return false;
+            }
+            
+            var Props = a.GetType().GetProperties();
+            
+            foreach (var Prop in Props)
+            {
+              
+                var aPropValue = Prop.GetValue(a);
+                var bPropValue = Prop.GetValue(b);
+                   
+                if (aPropValue?.ToString() != bPropValue?.ToString())
+                {
+                    return false;
+                }                    
+            }
+            return true;
+        }
+    }
+}
--- NetCoreRestApi/tests/UnitTests/WebApi/Controllers/ProductsControllerTests.cs
+++ NetCoreRestApi/tests/UnitTests/WebApi/Controllers/ProductsControllerTests.cs
@@ -2,11 +2,14 @@
 using Common.Converter;
 using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ServiceLayer.DataTransferObjects;
 using System.Collections.Generic;
 using System.Linq;
+using UnitTests.Helpers;
+using WebAPI;
 using WebAPI.Controllers;
 
 namespace UnitTests.WebApi.Controllers
@@ -81,24 +84,48 @@ namespace UnitTests.WebApi.Controllers
         public void ProductsController_Get_ReturnsStatusCode200()
         {
             // Arrange
-            var managerMock = new Mock<IProductManager>();
-            managerMock
-                .Setup(m => m.GetAll())
-                .Returns(_models);
+            var productModelsList = new List<ProductModel>()
+            {
+                new ProductModel()
+                {
+                    Id = 1,
+                    Name = "First"
+                },
+                new ProductModel()
+                {
+                    Id = 1,
+                    Name = null
+                }
+            };
 
-            var converterMock = new Mock<IConverter<ProductDto, ProductModel>>();
-            converterMock
-                .Setup(c => c.ConvertFrom(It.IsAny<IEnumerable<ProductModel>>()))
-                .Returns(_dtos);
-
-            var controller = new ProductsController(managerMock.Object, converterMock.Object);
-
-            // Act
-            var result = controller.Get();
-            var actual = result as OkObjectResult;
+            var x = new CategoryModel()
+            {
+                Id = 1,
+                Name = null
+            };
 
             // Assert            
-            Assert.AreEqual(200, actual.StatusCode);
+            Assert.IsTrue(ObjectPropertiesComparer.AreEqual(productModelsList.First(), productModelsList.Last()));
+        }
+
+        bool Equal(object a, object b)
+        {
+            // They're both null.
+            if (a == null && b == null) return true;
+            // One is null, so they can't be the same.
+            if (a == null || b == null) return false;
+            // How can they be the same if they're different types?
+            if (a.GetType() != b.GetType()) return false;
+            var Props = a.GetType().GetProperties();
+            foreach (var Prop in Props)
+            {
+                // See notes *
+                var aPropValue = Prop.GetValue(a) ?? string.Empty;
+                var bPropValue = Prop.GetValue(b) ?? string.Empty;
+                if (aPropValue.ToString() != bPropValue.ToString())
+                    return false;
+            }
+            return true;
         }
     }
 }
