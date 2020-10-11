02058254e92cf2324dbb0bb860048d5375f232ec
Zinko Viktor <zinkoexp@gmail.com>
2020-10-11 22:32:14 +0300

    dbcontex

---
+++ NetCoreRestApi/tests/UnitTests/BaseTest.cs
@@ -0,0 +1,68 @@
+﻿using BusinessLayer.Managers;
+using Common.Converter;
+using DataLayer.EF.Converters;
+using DataLayer.EF.Entities;
+using DataLayer.EF.Repositories;
+using DataLayer.Models;
+using DataLayer.Repositories;
+using DataLayer.UnitOfWorks;
+using DataLayer.UnitOfWorks.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLayer.Converters;
+using ServiceLayer.DataTransferObjects;
+using System;
+
+namespace UnitTests
+{
+    public abstract class BaseTest
+    {
+        public TestContext TestContext { get; set; }
+
+        public IServiceProvider ServiceProvider { get; set; }
+        private ServiceCollection _services;
+
+        public BaseTest()
+        {
+            InitializeBaseServices();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ServiceProvider = services.BuildServiceProvider();
+            
+        }
+
+        public void ConfigureServices()
+        {
+            ServiceProvider = _services.BuildServiceProvider();
+
+        }
+
+        private void InitializeBaseServices()
+        {
+            _services = new ServiceCollection();
+            _services.AddTransient<IProductManager, ProductManager>()
+                    .AddTransient<ICategoryManager, CategoryManager>()
+                    .AddTransient<IConverter<ProductDto, ProductModel>, ProductServiceConverter>()
+                    .AddTransient<IConverter<CategoryDto, CategoryModel>, CategoryServiceConverter>()
+                    .AddTransient<IConverter<ProductEntity, ProductModel>, ProductModelConverter>()
+                    .AddTransient<IConverter<CategoryEntity, CategoryModel>, CategoryModelConverter>()
+                    .AddTransient<IProductUnitOfWork, ProductUnitOfWork>()
+                    .AddTransient<ICategoryUnitOfWork, CategoryUnitOfWork>()
+                    .AddTransient<IProductRepository, ProductRepository>()
+                    .AddTransient<ICategoryRepository, CategoryRepository>();                      
+        }
+
+        public void InjectService(Type serviceType, Func<IServiceProvider, object> implementation)
+        {
+            _services.AddTransient(serviceType, implementation);
+        }
+
+        public void UseInMemoryDatabase()
+        {
+            _services.AddEntityFrameworkInMemoryDatabase();
+        }
+    }
+}
--- NetCoreRestApi/tests/UnitTests/WebApi/Controllers/ProductsControllerTests.cs
+++ NetCoreRestApi/tests/UnitTests/WebApi/Controllers/ProductsControllerTests.cs
@@ -1,25 +1,37 @@
 ﻿using BusinessLayer.Managers;
 using Common.Converter;
+using DataLayer.EF;
 using DataLayer.Models;
+using DataLayer.Repositories.Intefaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ServiceLayer.DataTransferObjects;
 using System.Collections.Generic;
 using System.Linq;
+using UnitTests.Helpers;
+using WebAPI;
 using WebAPI.Controllers;
 
 namespace UnitTests.WebApi.Controllers
 {
     [TestClass]
-    public class ProductsControllerTests
+    public class ProductsControllerTests : BaseTest, IDbContext
     {
+        IProductManager manager;
+        IConverter<ProductDto, ProductModel> converter;
+
         private IQueryable<ProductModel> _models;
         private IEnumerable<ProductDto> _dtos;
 
         [TestInitialize]
         public void TestInitialize()
         {
+            
+
             var productModelsList = new List<ProductModel>()
             {
                 new ProductModel()
@@ -49,23 +61,24 @@ namespace UnitTests.WebApi.Controllers
                 }
             };
             _dtos = productDtosList.AsQueryable();
+
+            var mock = new Mock<IDbContext>();
+            var dbSetMock = new Mock<DbSet<ProductDto>>();
+            dbSetMock.Setup(m => m.AsQueryable()).Returns(_dtos.AsQueryable());
+            mock.Setup(d => d.GetDbSet<ProductDto>()).Returns(GetQueryableMockDbSet<ProductDto>(_dtos.ToList()));
+            InjectService(typeof(IUnitOfWorkContext), mock.Object);
+            InjectService(typeof(IDbContext), mock.Object.GetType());
+            ConfigureServices();
+
+            manager = ServiceProvider.GetRequiredService<IProductManager>();
+            converter = ServiceProvider.GetRequiredService<IConverter<ProductDto, ProductModel>>();
         }
 
         [TestMethod]
         public void ProductsController_Get_ReturnsCategoryDtosList()
         {
             // Arrange
-            var managerMock = new Mock<IProductManager>();
-            managerMock
-                .Setup(m => m.GetAll())
-                .Returns(_models);
-
-            var converterMock = new Mock<IConverter<ProductDto, ProductModel>>();
-            converterMock
-                .Setup(c => c.ConvertFrom(It.IsAny<IEnumerable<ProductModel>>()))
-                .Returns(_dtos);
-
-            var controller = new ProductsController(managerMock.Object, converterMock.Object);
+            var controller = new ProductsController(manager, converter);
 
             var expected = _dtos;
 
@@ -81,24 +94,67 @@ namespace UnitTests.WebApi.Controllers
         public void ProductsController_Get_ReturnsStatusCode200()
         {
             // Arrange
-            var managerMock = new Mock<IProductManager>();
-            managerMock
-                .Setup(m => m.GetAll())
-                .Returns(_models);
+            var productModelsList = new List<ProductModel>()
+            {
+                new ProductModel()
+                {
+                    Id = 1,
+                    Name = "First"
+                },
+                new ProductModel()
+                {
+                    Id = 1,
+                    Name = null
+                }
+            };
 
-            var converterMock = new Mock<IConverter<ProductDto, ProductModel>>();
-            converterMock
-                .Setup(c => c.ConvertFrom(It.IsAny<IEnumerable<ProductModel>>()))
-                .Returns(_dtos);
-
-            var controller = new ProductsController(managerMock.Object, converterMock.Object);
-
-            // Act
-            var result = controller.Get();
-            var actual = result as OkObjectResult;
+            var x = new CategoryModel()
+            {
+                Id = 1,
+                Name = null
+            };
 
             // Assert            
-            Assert.AreEqual(200, actual.StatusCode);
+            Assert.IsTrue(ObjectPropertiesComparer.AreEqual(productModelsList.First(), productModelsList.Last()));
+        }
+
+        bool Equal(object a, object b)
+        {
+            // They're both null.
+            if (a == null && b == null) return true;
+            // One is null, so they can't be the same.
+            if (a == null || b == null) return false;
+            // How can they be the same if they're different types?
+            if (a.GetType() != b.GetType()) return false;
+            var Props = a.GetType().GetProperties();
+            foreach (var Prop in Props)
+            {
+                // See notes *
+                var aPropValue = Prop.GetValue(a) ?? string.Empty;
+                var bPropValue = Prop.GetValue(b) ?? string.Empty;
+                if (aPropValue.ToString() != bPropValue.ToString())
+                    return false;
+            }
+            return true;
+        }
+
+        private DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+
+            return dbSet.Object;
+        }
+
+        public int Save()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
