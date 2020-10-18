77c6e707ada16305c01f21e38d3ecb21c9134f46
Viktor Zinko <zinkoexp@gmail.com>
2020-10-18 22:13:31 +0300

    removed ordering

--- NetCoreRestApi/DataLayer/Models/BaseModel.cs
+++ NetCoreRestApi/DataLayer/Models/BaseModel.cs
@@ -1,10 +1,7 @@
-﻿using Newtonsoft.Json;
-
-namespace DataLayer.Models
+﻿namespace DataLayer.Models
 {
     public class BaseModel<TId>
     {
-        [JsonProperty(Order = 0)]
         public TId Id { get; set; }
     }
 }
