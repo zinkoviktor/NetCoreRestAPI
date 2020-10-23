fba2097806740d3c96e70a4f1e12cd2a9d282253
Viktor Zinko <zinkoexp@gmail.com>
2020-10-23 10:48:29 +0300

    UnitOfWorkTests (#18)
    * IRepositoryDbContext refactoring
    
    * added test, and refactored comparer
    
    * added CategoryServiceConverter convert
    
    * added product service converter tests
    
    * Added Managers and Conrtrolers tests
    
    * Added property comparer
    
    * dbcontex
    
    * Add DI to tests
    
    * important changes
    
    * Comparer tests
    
    * Added DI, Controler tests and Test Helpers
    
    * Cleanup
    
    * small refactoring
    
    * Fix after review
    
    * removed ordering
    
    * removed Newtonsoft.Json
    
    * added Product UnitOfWork tests
    
    * added test
    
    * add new test

---
+++ NetCoreRestApi/tests/UnitTests/DataLayer/UnitOfWorks/ProductUnitOfWorkTests.cs
@@ -0,0 +1,135 @@
+﻿using DataLayer.EF;
+using DataLayer.EF.Entities;
+using DataLayer.Models;
+using DataLayer.Repositories.Intefaces;
+using DataLayer.UnitOfWorks.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitTests.Helpers.ProductHelpers;
+
+namespace UnitTests.DataLayer.UnitOfWorks
+{
+    [TestClass]
+    public class ProductUnitOfWorkTests : BaseTest
+    {
+        private IProductUnitOfWork UnitOfWork { get; set; }  
+        private IDbContext DbContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {           
+            Services.AddSingleton<ProductsDbContext>()
+                    .AddSingleton<IUnitOfWorkContext>(x => x.GetRequiredService<ProductsDbContext>())
+                    .AddSingleton<IDbContext>(x => x.GetRequiredService<ProductsDbContext>())
+                    .AddDbContext<ProductsDbContext>(opt => opt.UseInMemoryDatabase(databaseName: 
+                        Guid.NewGuid().ToString()),ServiceLifetime.Scoped, ServiceLifetime.Scoped);
+
+            ConfigureServices();
+
+            UnitOfWork = ServiceProvider.GetRequiredService<IProductUnitOfWork>();
+            DbContext = ServiceProvider.GetRequiredService<IDbContext>();
+        }
+
+        [TestMethod]
+        public void Create_ProductEntity()
+        {
+            // Arrange
+            var products = new List<ProductModel>()
+            {
+                new ProductModel()
+                {
+                    Id = 1,
+                    Name = "HP 410",
+                    Description = "All-in-One Wireless Ink Tank Color Printer",
+                    Price = 90,
+                    AvailableCount = 9                    
+                },
+                new ProductModel()
+                {
+                    Id = 2,
+                    Name = "Epson L3152",
+                    Description = "WiFi All in One Ink Tank Printer",
+                    Price = 60,
+                    AvailableCount = 19                    
+                }
+            };
+            var expected = new List<ProductEntity>()
+            {
+                new ProductEntity()
+                {
+                    Id = 1,
+                    Name = "HP 410",
+                    Description = "All-in-One Wireless Ink Tank Color Printer",
+                    Price = 90,
+                    AvailableCount = 9,
+                    ProductCategoryEntities = new List<ProductCategoryEntity>()
+
+                },
+                new ProductEntity()
+                {
+                    Id = 2,
+                    Name = "Epson L3152",
+                    Description = "WiFi All in One Ink Tank Printer",
+                    Price = 60,
+                    AvailableCount = 19,
+                    ProductCategoryEntities = new List<ProductCategoryEntity>()
+                }
+            };
+
+            // Act
+            UnitOfWork.Create(products.AsEnumerable());
+            UnitOfWork.Save();
+            var actual = DbContext.GetDbSet<ProductEntity>().ToList();
+
+            // Assert     
+            Assert.IsTrue(ProductEntityComparer.Instance.AreEquals(expected, actual));
+        }
+
+        [TestMethod]
+        public void GetById_ProductEntity()
+        {
+            // Arrange
+            var products = new List<ProductEntity>()
+            {
+                new ProductEntity()
+                {
+                    Id = 1,
+                    Name = "HP 410",
+                    Description = "All-in-One Wireless Ink Tank Color Printer",
+                    Price = 90,
+                    AvailableCount = 9,
+                    ProductCategoryEntities = new List<ProductCategoryEntity>()
+                },
+                new ProductEntity()
+                {
+                    Id = 2,
+                    Name = "Epson L3152",
+                    Description = "WiFi All in One Ink Tank Printer",
+                    Price = 60,
+                    AvailableCount = 19,
+                    ProductCategoryEntities = new List<ProductCategoryEntity>()
+                }
+            };
+            var expected = new ProductModel()
+            {
+                Id = 1,
+                Name = "HP 410",
+                Description = "All-in-One Wireless Ink Tank Color Printer",
+                Price = 90,
+                AvailableCount = 9
+            };
+
+            // Act
+            DbContext.GetDbSet<ProductEntity>().AddRange(products);
+            DbContext.Save();
+            var actual = UnitOfWork.GetById(expected.Id);
+
+            // Assert     
+            Assert.IsTrue(ProductModelComparer.Instance.AreEquals(expected, actual));
+        }
+    }
+}
